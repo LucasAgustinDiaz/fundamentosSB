1c0be14372ced9b7deff4876ca136467115580b4
LucasAgustinDiaz <lucasagustin.diaz@mercadolibre.com>
2021-06-27 02:50:34 -0300

    first commit

---
+++ src/main/java/bootcamp/demo/FundamentosApplication.java
@@ -0,0 +1,21 @@
+package bootcamp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentosApplication.class, args);
+    }
+
+}
+/**
+ * git init
+ * git add README.md
+ * git commit -m "first commit"
+ * git branch -M main
+ * git remote add origin git@github.com:LucasAgustinDiaz/fundamentosSB.git
+ * git push -u origin main
+ */
